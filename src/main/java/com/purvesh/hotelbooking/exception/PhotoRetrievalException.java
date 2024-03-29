c1dd2ee3d79a4230ea15afd336cfef99959a6d2a
Purvesh Krishnani <69525951+puru763@users.noreply.github.com>
2024-03-29 14:12:37 +0530

    Update PhotoRetrievalException.java

--- src/main/java/com/purvesh/hotelbooking/exception/PhotoRetrievalException.java
+++ src/main/java/com/purvesh/hotelbooking/exception/PhotoRetrievalException.java
@@ -1,8 +1,6 @@
 package com.purvesh.hotelbooking.exception;
 
-/**
- * @author Simpson Alfred
- */
+
 
 public class PhotoRetrievalException extends RuntimeException {
     public PhotoRetrievalException(String message) {
