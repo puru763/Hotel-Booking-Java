09c78d6b73dc7172d7d7c2fb45a40e3aa3697f28
Purvesh Krishnani <69525951+puru763@users.noreply.github.com>
2024-04-02 13:32:38 +0530

    Update InvalidBookingRequestException.java

--- src/main/java/com/purvesh/hotelbooking/exception/InvalidBookingRequestException.java
+++ src/main/java/com/purvesh/hotelbooking/exception/InvalidBookingRequestException.java
@@ -1,8 +1,6 @@
 package com.purvesh.hotelbooking.exception;
 
-/**
- * @author Simpson Alfred
- */
+
 
 public class InvalidBookingRequestException extends RuntimeException {
     public InvalidBookingRequestException(String message) {
