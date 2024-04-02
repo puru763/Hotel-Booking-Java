b14247c5de1daebf868f9b18856b73368e00cb17
Purvesh Krishnani <69525951+puru763@users.noreply.github.com>
2024-04-02 13:31:21 +0530

    Update AuthController.java

--- src/main/java/com/purvesh/hotelbooking/controller/AuthController.java
+++ src/main/java/com/purvesh/hotelbooking/controller/AuthController.java
@@ -24,9 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 
-/**
- * @author Simpson Alfred
- */
+
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
