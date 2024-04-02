b5ce66b68921aba3945df258383ca5ffbecc5e4d
Purvesh Krishnani <69525951+puru763@users.noreply.github.com>
2024-04-02 13:33:58 +0530

    Update RoleRepository.java

--- src/main/java/com/purvesh/hotelbooking/repository/RoleRepository.java
+++ src/main/java/com/purvesh/hotelbooking/repository/RoleRepository.java
@@ -5,9 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-/**
- * @author Simpson Alfred
- */
+
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
