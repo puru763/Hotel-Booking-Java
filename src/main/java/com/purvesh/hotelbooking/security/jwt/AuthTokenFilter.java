77e7f346532c2610aaf5c6666a57e35a0384fe1c
Purvesh Krishnani <69525951+puru763@users.noreply.github.com>
2024-04-02 13:35:24 +0530

    Update AuthTokenFilter.java

--- src/main/java/com/purvesh/hotelbooking/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/purvesh/hotelbooking/security/jwt/AuthTokenFilter.java
@@ -18,9 +18,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-/**
- * @author Simpson Alfred
- */
+
 
 public class AuthTokenFilter extends OncePerRequestFilter {
 
